fb83cf2e8e4eb4b3280524bf66b6a3ceedd80194
yusuf <yusuffront@gmail.com>
2021-10-07 23:18:53 +0800

    create new folder --testing

---
+++ small2/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('container')
+    <h4>Hello</h4>
+@endsection
--- small2/routes/web.php
+++ small2/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function(){
+    return view('home', [
+        "title"=>"Home Page"
+    ]);
 });
+
